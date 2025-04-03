cd8d39ca291652cdfb71c32beb390e1907d46adc
adrianmurage <adrianmurage21@gmail.com>
2025-04-03 22:13:26 +0300

    feat: integrate PostHog for enhanced analytics tracking
    - Added PostHogProvider to the main application for improved user behavior tracking.
    - Updated package.json and package-lock.json to include posthog-js dependency.
    - Modified .gitignore to include .env.local for local environment configuration.

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { PostHogProvider } from 'posthog-js/react';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <App />
+        <PostHogProvider
+            apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+            options={{
+                api_host: "https://eu.i.posthog.com",
+            }}
+        >
+            <App />
+        </PostHogProvider>
     </StrictMode>
 );
